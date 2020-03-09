1bde77b75685ab2597dcbfddf581c677d98de7bf
Boureima Tapily <boureimatapily1@gmail.com>
2020-03-09 00:45:19 +0000

    ADD Contact and Infos

---
+++ src/Components/Contact.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import {Container, Row, Col, } from 'react-bootstrap'
+
+const Contact = () => {
+  return (
+    <div>
+      <div className="headerContent">
+      <Container>
+        <Row>
+          <Col >
+            <p>
+              <h2>TAKAMA</h2>
+              TAKAMA est un site de vente de ticket en ligne qui facile l'achat 
+              et la reservation de vos tickets de voyage
+            </p>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col >
+            <h1>hello</h1>
+          </Col>
+          <Col >
+            <h1>hello</h1>
+        </Col>
+        </Row>
+      
+      </Container>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
