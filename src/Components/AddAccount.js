a3fdd39ce0f62d0b88c392f562fe94324a2c0da6
Boureima Tapily <boureimatapily1@gmail.com>
2020-03-09 00:37:29 +0000

    ADD Addaccount +Editaccount

--- src/Components/AddAccount.js
+++ src/Components/AddAccount.js
@@ -12,42 +12,110 @@ class AddAccount extends React.Component{
         let account = {
            // id: Math.random().toString(36).substr(2, 5),
             userName:event.target.elements.userName.value,
+            depart:event.target.elements.depart.value,
+            arrival:event.target.elements.arrival.value,
+            colis:event.target.elements.colis.value,
+            date:event.target.elements.date.value,
+            num:event.target.elements.num.value,
+            
         }
+        console.log(account)
         this.props.addNewAccount(account)
-        // this.props.history.push('/')
+        this.props.history.push('/')
     }
 
         render(){
            
             return(
                 <div>
-
+                        <div className="headerContent">
                 <Container>
+               <br/> <Link to="/"><Button>Retour</Button></Link>
                     <Row>
 
-                        <Col  lg={{offset: 3 }} >
+                        <Col  lg={{offset: 2 }} >
                             <Form onSubmit={this.addAccount}>
                                 <Form.Group as={Row} controlId="userName">
-                                    <Form.Label column lg={2}>
-                                       User Name
-                                     </Form.Label>
+                                    <Form.Label column lg={3}>
+                                       Nom / Name
+                                     </Form.Label><br />
                                     <Col lg={4}>
                                         <Form.Control type="text" name="userName"
-                                             placeholder="User Name"
+                                             placeholder="Votre Nom / Your Name"
                                              />
-                                    </Col>
-                                    <Button variant="primary" type="submit">Add Name</Button>
+                                    </Col>                                
                                 </Form.Group>
-                                <Col className="d-flex justify-content-center">
-                            
-                        </Col>
+
+                                <Form.Group as={Row} controlId="num">
+                                    <Form.Label column lg={3}>
+                                    N. Telephone / Phone Number
+                                     </Form.Label><br />
+                                    <Col lg={4}>
+                                        <Form.Control type="number" name="num"
+                                              placeholder="+22376124885"
+                                             />
+                                    </Col>                                
+                                </Form.Group>
+
+                                <Form.Group as={Row} controlId="depart">
+                                    <Form.Label column lg={3}>
+                                       Lieu de Depart / Departure
+                                     </Form.Label><br />
+                                    <Col lg={4}>
+                                        <Form.Control as="select" name="depart">
+                                        <option >Clicker pour choisir</option>
+                                        <option >Accra</option>
+                                        <option >Kumasi</option>
+                                        <option >Bamako</option>
+                                        </Form.Control>
+                                    </Col>                                
+                                </Form.Group>
+
+                                <Form.Group as={Row} controlId="arrival">
+                                    <Form.Label column lg={3}>
+                                       Lieu d'Arriver / Arrival
+                                     </Form.Label><br />
+                                    <Col lg={4}>
+                                        <Form.Control as="select" name="arrival">
+                                        <option >Clicker pour choisir</option>
+                                        <option >Accra</option>
+                                        <option >Kumasi</option>
+                                        <option >Bamako</option>
+                                        </Form.Control>
+                                    </Col>                                
+                                </Form.Group>
+
+                                <Form.Group as={Row} controlId="colis">
+                                    <Form.Label column lg={3}>
+                                       Nombre de colis
+                                     </Form.Label><br />
+                                    <Col lg={4}>
+                                        <Form.Control type="number" name="colis"
+                                             placeholder="0"
+                                             />
+                                    </Col>                                
+                                </Form.Group>
+
+                                <Form.Group as={Row} controlId="date">
+                                    <Form.Label column lg={3}>
+                                       Date de Depart
+                                     </Form.Label><br />
+                                    <Col lg={4}>
+                                        <Form.Control type="date" name="date"
+                                             
+                                             />
+                                    </Col>                                
+                                </Form.Group>
+
+
+                                <Button variant="primary" type="submit">Ajouter</Button>
                             </Form>
                         </Col>
 
                     </Row>
                 </Container>
 
-
+                </div>
                 </div>
 
             )
--- src/Components/EditAccount.js
+++ src/Components/EditAccount.js
@@ -11,6 +11,12 @@ class EditAccount extends React.Component {
         let account = {
             id: this.props.match.params.id,
             userName:event.target.elements.userName.value,
+            depart:event.target.elements.depart.value,
+            arrival:event.target.elements.arrival.value,
+            colis:event.target.elements.colis.value,
+            date:event.target.elements.date.value,
+            num:event.target.elements.num.value,
+
         }
         this.props. editAccount(account)
         this.props.history.push('/')
@@ -26,32 +32,94 @@ class EditAccount extends React.Component {
 
         return (
             <div>
-
+                        <div className="headerContent"> <br/>
                 <Container>
                     <Row>
 
-                        <Col lg={{ offset: 3 }} >
-                            <Form onSubmit={this.updateAccount}> 
+                        <Col lg={{ offset: 2 }} >
+                            <Form onSubmit={this.updateAccount}>
+
                                 <Form.Group as={Row} controlId="userName">
-                                    <Form.Label column lg={2}>
-                                       User Name
-                                     </Form.Label>
+                                    <Form.Label column lg={3}>
+                                       Nom / Name
+                                     </Form.Label><br />
                                     <Col lg={4}>
-                                        <Form.Control type="text" name="usertName"
-                                         defaultValue={account.userName}
-                                            placeholder="User Name"
-                                        />
-                                    </Col>
- <Button variant="primary" type="Submit">Update Name</Button>   |    <Link to="/"> <Button variant="success">CANCEL</Button> </Link>
+                                        <Form.Control type="text" name="userName"
+                                              defaultValue={account.userName}
+                                            
+                                             />
+                                    </Col>                                
                                 </Form.Group>
+
+                                <Form.Group as={Row} controlId="num">
+                                    <Form.Label column lg={3}>
+                                    N. Telephone / Phone Number
+                                     </Form.Label><br />
+                                    <Col lg={4}>
+                                        <Form.Control type="number" name="num"
+                                             defaultValue={account.num}
+                                             />
+                                    </Col>                                
+                                </Form.Group>
+
+
+                                <Form.Group as={Row} controlId="depart">
+                                    <Form.Label column lg={3}>
+                                       Lieu de Depart / Departure
+                                     </Form.Label><br />
+                                    <Col lg={4}>
+                                        <Form.Control as="select" name="depart"
+                                         defaultValue={account.depart}>
+                                        <option >Clicker pour choisir</option>
+                                        <option >Accra</option>
+                                        <option >Kumasi</option>
+                                        <option >Bamako</option>
+                                        </Form.Control>
+                                    </Col>                                
+                                </Form.Group>
+
+                                <Form.Group as={Row} controlId="arrival">
+                                    <Form.Label column lg={3}>
+                                       Lieu d'Arriver / Arrival
+                                     </Form.Label><br />
+                                    <Col lg={4}>
+                                        <Form.Control as="select" name="arrival"
+                                         defaultValue={account.arrival}>
+                                        <option >Clicker pour choisir</option>
+                                        <option >Accra</option>
+                                        <option >Kumasi</option>
+                                        <option >Bamako</option>
+                                        </Form.Control>
+                                    </Col>                                
+                                </Form.Group>
+
+                                <Form.Group as={Row} controlId="colis">
+                                    <Form.Label column lg={3}>
+                                       Nombre de colis
+                                     </Form.Label><br />
+                                    <Col lg={4}>
+                                        <Form.Control type="number" name="colis"
+                                         defaultValue={account.colis}/>
+                                    </Col>                                
+                                </Form.Group>
+
+                                <Form.Group as={Row} controlId="date">
+                                    <Form.Label column lg={3}>
+                                       Date de Depart
+                                     </Form.Label><br />
+                                    <Col lg={4}>
+                                        <Form.Control type="date" name="date"
+                                              defaultValue={account.date}
+                                             />
+                                    </Col>                                
+                                </Form.Group>
+                                <Button variant="primary" type="Submit">Modifier</Button>   |    <Link to="/"> <Button variant="success">Annuler</Button> </Link>
                             </Form>
                         </Col>
 
                     </Row>
                 </Container>
-                
-
-
+                </div>
             </div>
 
         )
